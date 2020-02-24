8b47fef8f4b69063f06f55c8ae2a33e86bdfaa5d
Egor Chopchits <hgooglik@gmail.com>
2020-02-24 10:00:14 +0300

    Created object of class in method Main

---
+++ Laba1/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Adress
+    {
+       private string city { get; set; } 
+       private string street { get; set; }
+       private int nomberOfHouse { get; set; }
+    }
+}
